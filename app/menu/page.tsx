8abbb3ba91d8f31830026377882c6af29873b29c
Dustin Ingram <dustiningram@Dustins-Mac-mini.local>
2026-09-24 14:16:04 -0500

    Add The Loo team store preview

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -24,6 +24,9 @@ export default function Menu() {
           <a href="/recruiting/">
             Recruiting <span>→</span>
           </a>
+          <a href="/store/">
+            The Loo Team Store <span>→</span>
+          </a>
           <a href="/#media">
             Media <span>→</span>
           </a>
